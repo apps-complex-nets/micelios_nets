d9e3e4cd0d667ff319826141f525cb23b20aaa46
Daniela Aguirre Guerrero <daguirre@cua.uam.mx>
2025-09-25 10:16:44 -0600

    first commit

---
+++ .ipynb_checkpoints/constants-checkpoint.py
@@ -0,0 +1,5 @@
+PATH_TRICHODERMA = "data/Trichoderma_atroviride/Muestra"
+PATH_NEUROSPORA = "data/Neurospora_crassa/Muestra"
+PATH_NETS_TRICHODERMA = "redes/Trichoderma_atroviride/"
+PATH_NETS_NEUROSPORA = "redes/Neurospora_crassa/"
+NUM_MUESTRAS = 5
